c0d391a128c076c3daf1706fc0a886aeb0c453e3
atutby <sasha2015to@gmail.com>
2020-11-22 10:43:38 +0300

    iframe-patern.js best

--- js/iframe-patern.js
+++ js/iframe-patern.js
@@ -1,37 +1,47 @@
 let item2s = document.getElementsByClassName("item2");
 item2s = Array.from(item2s);
 
-const regexp = /\w{11}/;
+const regexp = /[\w-]{11}/;
 let titleItem, ulItem;
 
 item2s.forEach((item) => {
   idYoutube = item.firstElementChild.getAttribute("href").match(regexp)[0];
-	
-	titleItem = item.firstElementChild.innerText;
 
+  titleItem = item.firstElementChild.innerText;
 
-	ulItem = item.firstElementChild.nextElementSibling.classList.add("crossing");
-	ulItem = item.firstElementChild.nextElementSibling.insertAdjacentHTML('afterbegin', '<li class="control">Развернуть/Свернуть</li>');
-	ulItem = item.firstElementChild.nextElementSibling.outerHTML;
+  let ul;
+  ul = item.firstElementChild.nextElementSibling.classList.add("crossing");
+  ul = item.firstElementChild.nextElementSibling.insertAdjacentHTML(
+    "afterbegin",
+    '<li class="control">Развернуть/Свернуть</li>'
+  );
+  ul = item.firstElementChild.nextElementSibling.outerHTML;
 
   item.innerHTML = `
-<div class="item">
-	<h2><a href="https://www.youtube.com/watch?v=${idYoutube}">${titleItem}</a></h2>
-	<div>
-		<div class="img">
-			<div class="video12">
-				<iframe
-				src="https://www.youtube.com/embed/${idYoutube}"
-				frameborder="0"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-				allowfullscreen
-			></iframe>
+			<div class="item">
+				<h2><a href="https://www.youtube.com/watch?v=${idYoutube}">${titleItem}</a></h2>
+				<div>
+					<div class="img">
+						<div class="video12" style="background: url(//i.ytimg.com/vi/${idYoutube}/hqdefault.jpg)  0% 0% / cover;">
+								<div class="video1-play" onclick="videoplay(this, '${idYoutube}');"></div>
+						</div>
+					</div>
+					${ul}
+				</div>
 			</div>
-		</div>
-		${ulItem}
-	</div>
-</div>
-`;
-}); 
+			`;
+});
 // - the End cycle intem2s.forEach
-console.log(idYoutube + ' cat');
+
+function videoplay(button, id) {
+	var par = button.parentNode;
+	console.log(par);
+    par.innerHTML = `
+				<iframe
+							src="https://www.youtube.com/embed/${id}?autoplay=1&rel=0"
+							frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen
+					>
+				</iframe>`;
+}
