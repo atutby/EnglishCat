513369b054f6093c4fc9a5dbb6ef4003dc4628b7
atutby <sasha2015to@gmail.com>
2020-11-25 15:38:16 +0300

    THANKSGIVING

--- js/iframe-patern.js
+++ js/iframe-patern.js
@@ -2,41 +2,44 @@ let item2s = document.getElementsByClassName("item2");
 item2s = Array.from(item2s);
 
 const regexp = /[\w-]{11}/;
-let titleItem;
+
+let titleItem, idYoutube, href;
 
 item2s.forEach((item) => {
-  idYoutube = item.firstElementChild.getAttribute("href").match(regexp)[0];
+  href = item.getElementsByTagName("a")[0].getAttribute("href");
+  idYoutube = href.match(regexp)[0];
 
-  titleItem = item.firstElementChild.innerText;
+  titleItem = item.getElementsByTagName("a")[0].innerText;
 
-  let ul;
+  let ul = item.getElementsByTagName("ul")[0];
   // ul = item.firstElementChild.nextElementSibling.classList.add("crossing");
   // ul = item.firstElementChild.nextElementSibling.insertAdjacentHTML(
   //   "afterbegin",
   //   '<li class="control">Развернуть/Свернуть</li>'
   // );
-  ul = item.firstElementChild.nextElementSibling.outerHTML;
 
-  item.innerHTML = `
-			<div class="item">
-				<h2><a href="https://www.youtube.com/watch?v=${idYoutube}">${titleItem}</a></h2>
+  item.outerHTML = `
+			<div class="item2">
+				<h2><a href="${href}">${titleItem}</a></h2>
 				<div>
 					<div class="img">
 						<div class="video12" style="background: url(//i.ytimg.com/vi/${idYoutube}/hqdefault.jpg)  0% 0% / cover;">
 								<div class="video1-play" onclick="videoplay(this, '${idYoutube}');"></div>
 						</div>
 					</div>
-					${ul}
+					${ul.outerHTML}
 				</div>
 			</div>
 			`;
 });
 // - the End cycle intem2s.forEach
 
+
+// Заменить картинкой видео и кнопкой PLAY
 function videoplay(button, id) {
-	var par = button.parentNode;
-	console.log(par);
-    par.innerHTML = `
+  var par = button.parentNode;
+  console.log(par);
+  par.innerHTML = `
 				<iframe
 							src="https://www.youtube.com/embed/${id}?autoplay=1&rel=0"
 							frameborder="0"
@@ -45,3 +48,4 @@ function videoplay(button, id) {
 					>
 				</iframe>`;
 }
+// -- the End Заменить картинкой
