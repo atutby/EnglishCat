6aab25d6e7dcb4bcebeb31cd263745652d16abc6
atutby <sasha2015to@gmail.com>
2020-12-01 17:29:49 +0300

    защита от undefined элементов

--- js/iframe-patern.js
+++ js/iframe-patern.js
@@ -6,17 +6,23 @@ const regexp = /[\w-]{11}/;
 let titleItem, idYoutube, href;
 
 item2s.forEach((item) => {
-  href = item.getElementsByTagName("a")[0].getAttribute("href");
-  idYoutube = href.match(regexp)[0];
+	href = item.getElementsByTagName("a")[0].getAttribute("href");
+	if(!href) return; // защцита от пустой ссылки
+	idYoutube = href.match(regexp)[0];
+	if(!idYoutube) return; // защита от не найденного idYoutube
 
   titleItem = item.getElementsByTagName("a")[0].innerText;
-
+	if (!titleItem) { titleItem = 'Simple' }
   let ul = item.getElementsByTagName("ul")[0];
+	if (!ul) { // защита от пустого ul
+		ul = 'simple'
+	};
+
   // ul = item.firstElementChild.nextElementSibling.classList.add("crossing");
   // ul = item.firstElementChild.nextElementSibling.insertAdjacentHTML(
   //   "afterbegin",
   //   '<li class="control">Развернуть/Свернуть</li>'
-  // );
+	// );
 
   item.outerHTML = `
 			<div class="item2">
