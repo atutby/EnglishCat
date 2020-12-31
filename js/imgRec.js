d554a7c4de6fa4797c2886bc45555901a640eccf
atutby <sasha2015to@gmail.com>
2020-12-31 14:15:20 +0300

    by_the_skin_of_my_teeth.m4a

--- js/imgRec.js
+++ js/imgRec.js
@@ -1,5 +1,27 @@
-let imgRec = document.getElementById('imgRec');
+let audioBottom = document.getElementsByClassName("audio-bottom-off");
 
-imgRec.addEventListener("click", function(){
-	alert('Sorry this function not realised yet');
-});
+let imgRecText = `
+<div id="imgRec" class="imgRec" name="imgRec">
+Sorry this function not realised yet
+<img
+	src="./images/iconfinder_record_1419126.png"
+	alt="Button Record Voice"
+/>
+<img
+	src="./images/iconfinder_document_file_paper_page-10_2850898.png"
+	alt="Button Record Voice"
+/>
+</div>
+`;
+
+for (key in audioBottom){
+	audioBottom[key].insertAdjacentHTML("beforebegin", imgRecText);
+}
+
+// let imgRecs = document.getElementsByName('imgRec');
+
+// for (key in imgRecs){
+// 	imgRecs[key].addEventListener("click", function(){
+// 		alert('Sorry this function not realised yet');
+// 	})
+// }
